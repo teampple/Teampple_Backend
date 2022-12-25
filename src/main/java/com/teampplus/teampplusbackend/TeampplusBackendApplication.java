5f66264f6c46e8b86c9a4517e5f0b960c1cd15a1
squirMM <jsm6616@naver.com>
2022-12-26 02:21:02 +0900

    :rocket: upload local files

---
+++ src/main/java/com/teampplus/teampplusbackend/TeampplusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teampplus.teampplusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampplusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeampplusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teampplus/teampplusbackend/TeampplusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teampplus.teampplusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampplusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
