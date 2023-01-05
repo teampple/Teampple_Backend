e5f0df0158e6bf1ffe1b47eeeccb39d12964bd1a
squirMM <jsm6616@naver.com>
2023-01-05 23:40:15 +0900

    :rocket: userService function declaration #13

--- src/main/java/Backend/teampple/domain/users/UserController.java
+++ src/main/java/Backend/teampple/domain/users/UserController.java
@@ -1,2 +1,15 @@
-package Backend.teampple.domain.users;public class UserController {
+package Backend.teampple.domain.users;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/users", produces = "application/json; charset=utf-8")
+public class UserController {
+    private final UserService userService;
+
+
 }
