6a9b25646774da638b761cebb200832345084e98
squirMM <jsm6616@naver.com>
2023-01-07 20:24:47 +0900

    :rocket: add UserMapStruct #19

--- src/main/generated/Backend/teampple/domain/users/mapper/UserProfileMapperImpl.java
+++ src/main/generated/Backend/teampple/domain/users/mapper/UserProfileMapperImpl.java
@@ -1,13 +1,14 @@
 package Backend.teampple.domain.users.mapper;
 
 import Backend.teampple.domain.users.dto.UserProfileDto;
+import Backend.teampple.domain.users.entity.SubscriptionType;
 import Backend.teampple.domain.users.entity.UserProfile;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-07T20:00:08+0900",
+    date = "2023-01-07T20:04:26+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.15 (OpenLogic)"
 )
 @Component
@@ -42,6 +43,16 @@ public class UserProfileMapperImpl implements UserProfileMapper {
 
         UserProfile.UserProfileBuilder userProfile = UserProfile.builder();
 
+        userProfile.name( d.getName() );
+        userProfile.email( d.getEmail() );
+        userProfile.profileImage( d.getProfileImage() );
+        userProfile.schoolName( d.getSchoolName() );
+        userProfile.major( d.getMajor() );
+        userProfile.entranceYear( d.getEntranceYear() );
+        if ( d.getSubscribePlan() != null ) {
+            userProfile.subscribePlan( Enum.valueOf( SubscriptionType.class, d.getSubscribePlan() ) );
+        }
+
         return userProfile.build();
     }
 
@@ -49,6 +60,28 @@ public class UserProfileMapperImpl implements UserProfileMapper {
     public void updateFromDto(UserProfileDto d, UserProfile entity) {
         if ( d == null ) {
             return;
+        }
+
+        if ( d.getName() != null ) {
+            entity.setName( d.getName() );
+        }
+        if ( d.getEmail() != null ) {
+            entity.setEmail( d.getEmail() );
+        }
+        if ( d.getProfileImage() != null ) {
+            entity.setProfileImage( d.getProfileImage() );
+        }
+        if ( d.getSchoolName() != null ) {
+            entity.setSchoolName( d.getSchoolName() );
+        }
+        if ( d.getMajor() != null ) {
+            entity.setMajor( d.getMajor() );
+        }
+        if ( d.getEntranceYear() != null ) {
+            entity.setEntranceYear( d.getEntranceYear() );
+        }
+        if ( d.getSubscribePlan() != null ) {
+            entity.setSubscribePlan( Enum.valueOf( SubscriptionType.class, d.getSubscribePlan() ) );
         }
     }
 }
--- src/main/java/Backend/teampple/domain/users/mapper/UserProfileMapper.java
+++ src/main/java/Backend/teampple/domain/users/mapper/UserProfileMapper.java
@@ -1,2 +1,11 @@
-package Backend.teampple.domain.users.mapper;public interface UserProfileMapper {
+package Backend.teampple.domain.users.mapper;
+
+import Backend.teampple.domain.users.dto.UserProfileDto;
+import Backend.teampple.domain.users.entity.UserProfile;
+import Backend.teampple.global.common.mapper.GenericMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserProfileMapper extends GenericMapper<UserProfileDto, UserProfile> {
 }
