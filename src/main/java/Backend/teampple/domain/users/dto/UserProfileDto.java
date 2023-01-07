70940534448916eee48bf62f5acd243d1696756d
squirMM <jsm6616@naver.com>
2023-01-07 20:28:52 +0900

    :rocket: create dto #13

--- src/main/java/Backend/teampple/domain/users/dto/UserProfileDto.java
+++ src/main/java/Backend/teampple/domain/users/dto/UserProfileDto.java
@@ -1,2 +1,17 @@
-package Backend.teampple.domain.users.dto;public class UserProfileDto {
+package Backend.teampple.domain.users.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserProfileDto {
+    private String name;
+    private String email;
+    private String profileImage;
+    private String schoolName;
+    private String major;
+    private String entranceYear;
+    private String subscribePlan;
 }
