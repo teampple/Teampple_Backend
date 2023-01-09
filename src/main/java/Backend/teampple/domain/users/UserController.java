da4470a650a699718298e5df7b0673fdd228dd78
squirMM <jsm6616@naver.com>
2023-01-10 00:34:51 +0900

    :rocket: make controller #13

--- src/main/java/Backend/teampple/domain/users/UserController.java
+++ src/main/java/Backend/teampple/domain/users/UserController.java
@@ -1,15 +1,33 @@
 package Backend.teampple.domain.users;
 
+import Backend.teampple.domain.users.dto.UserProfileDto;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.Valid;
+
+@RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/users", produces = "application/json; charset=utf-8")
+@RequestMapping(value = "/users")
+@Api(tags = "사용자")
 public class UserController {
     private final UserService userService;
 
+    @PostMapping(value = "")
+    public UserProfileDto signUp(@Valid @RequestBody UserProfileDto userProfileDto){
+        return userService.signUp(userProfileDto);
+    }
+
+    @GetMapping("userprofiles")
+    public UserProfileDto getProfiles(Long id) {
+        return userService.getUserProfile(id);
+    }
+
+    @PutMapping("userprofiles")
+    public UserProfileDto updateProfile(Long id, @RequestBody UserProfileDto userProfileDto) {
+        return userService.updateUserProfile(id, userProfileDto);
+    }
 
 }
