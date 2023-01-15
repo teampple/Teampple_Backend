9bec2377894405e54439b9f091a38859b0e05d8e
wjdtkdgns <wjdtkdgns329@naver.com>
2023-01-15 23:50:11 +0900

    🚀 feat(invitation) : redis 셋팅 #38

---
+++ src/main/java/Backend/teampple/global/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package Backend.teampple.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    // redis와 connection을 생성
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    // RedisConnection에서 넘겨준 byte 값을 객체 직렬화
+    @Bean
+    public RedisTemplate<?,?> redisTemplate(){
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        // redis-cli을 통해 직접 데이터를 보려고 할 때 알아볼 수 없는 형태로 출력 방지
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
