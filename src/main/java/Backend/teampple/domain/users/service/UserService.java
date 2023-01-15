ea9f8a6ae639de9a5981419ee8aa583aa91435f6
squirMM <jsm6616@naver.com>
2023-01-15 20:37:02 +0900

    :rocket: add kakaoId params...#14

--- src/main/java/Backend/teampple/domain/users/service/UserService.java
+++ src/main/java/Backend/teampple/domain/users/service/UserService.java
@@ -3,7 +3,9 @@ package Backend.teampple.domain.users.service;
 import Backend.teampple.domain.users.entity.UserProfile;
 
 public interface UserService {
-    void createUser(UserProfile userProfile);
+    void createUser(UserProfile userProfile, String kakaoId, String refreshToken);
+
+    void updateUserRefreshToken(String kakaoId, String refreshToken);
 
     void deleteUserRefreshToken(String refreshToken);
 
--- src/main/java/Backend/teampple/domain/users/service/UserServiceImpl.java
+++ src/main/java/Backend/teampple/domain/users/service/UserServiceImpl.java
@@ -17,36 +17,52 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserProfileService userProfileService;
 
+    @Override
     @Transactional
-    public void createUser(UserProfile userProfile) {
-        User.builder()
+    public void createUser(UserProfile userProfile, String kakaoId, String refreshToken) {
+        User user = User.builder()
                 .userProfile(userProfile)
-//                .refreshToken()
+                .refreshToken(refreshToken)
                 .expRT(LocalDateTime.now().plusWeeks(2L))
+                .kakaoId(kakaoId)
                 .build();
+        userRepository.save(user);
     }
 
     @Override
+    @Transactional
+    public void updateUserRefreshToken(String kakaoId, String refreshToken) {
+        User user = userRepository.findByKakaoId(kakaoId)
+                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 유저 입니다"));
+        user.updateRefreshToken(refreshToken);
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
     public void deleteUserRefreshToken(String refreshToken) {
         User user = userRepository.findByRefreshToken(refreshToken)
-                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 유저 입니다"));
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 토큰입니다."));
         user.deleteRefreshToken();
         userRepository.save(user);
     }
 
+    @Override
     @Transactional
     public void deleteUser(String refreshToken){
         User user = userRepository.findByRefreshToken(refreshToken)
-                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 유저 입니다"));
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 토큰입니다."));
         userProfileService.deleteUserProfile(user.getUserProfile());
         user.updateIsDeleted();
         userRepository.save(user);
     }
 
+    @Override
     public String getTasks(String refreshToken) {
         return "";
     }
 
+    @Override
     public String getFeedbacks(String refreshToken) {
         return "";
     }
