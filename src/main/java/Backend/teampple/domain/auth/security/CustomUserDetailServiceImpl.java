377a94ee72b3e0e1a13345d44d1e179422b9419c
squirMM <jsm6616@naver.com>
2023-01-16 21:40:58 +0900

    :rocket: feat(auth) custom security.core.userdetails/userdetailService #14

--- src/main/java/Backend/teampple/domain/auth/security/CustomUserDetailServiceImpl.java
+++ src/main/java/Backend/teampple/domain/auth/security/CustomUserDetailServiceImpl.java
@@ -1,13 +1,17 @@
 package Backend.teampple.domain.auth.security;
 
+import Backend.teampple.domain.users.entity.User;
 import Backend.teampple.domain.users.repository.UserRepository;
 import Backend.teampple.global.error.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +24,12 @@ public class CustomUserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findById(Long.parseLong(username))
+        User user = userRepository.findByKakaoId(username)
                 .orElseThrow(() -> new NotFoundException("[" + username + "] 사용자를 찾을 수 없습니다."));
+        return CustomUserDetails.builder()
+                .kakaoId(user.getKakaoId())
+                .isDeleted(user.isDeleted())
+                .authorities(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+                .build();
     }
 }
--- src/main/java/Backend/teampple/domain/auth/security/CustomUserDetails.java
+++ src/main/java/Backend/teampple/domain/auth/security/CustomUserDetails.java
@@ -1,2 +1,57 @@
-package Backend.teampple.domain.auth.security;public class CustomUserDetails {
+package Backend.teampple.domain.auth.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private String kakaoId;
+    private boolean isDeleted;
+    private Collection<GrantedAuthority> authorities;
+
+    @Builder
+    public CustomUserDetails(String kakaoId, boolean isDeleted, Collection<GrantedAuthority> authorities) {
+        this.kakaoId = kakaoId;
+        this.isDeleted = isDeleted;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return kakaoId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isDeleted;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !isDeleted;
+    }
 }
