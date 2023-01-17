4ce5443cd649b0cf3f072be039c5d108b0388e39
squirMM <jsm6616@naver.com>
2023-01-17 22:38:01 +0900

    :hammer: refact(userController) add ApiOperation value #45

--- src/main/java/Backend/teampple/domain/users/UserController.java
+++ src/main/java/Backend/teampple/domain/users/UserController.java
@@ -5,6 +5,7 @@ import Backend.teampple.domain.users.dto.response.GetUserProfileDto;
 import Backend.teampple.domain.users.service.UserProfileService;
 import Backend.teampple.global.common.response.CommonResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -17,12 +18,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserProfileService userProfileService;
 
+    @ApiOperation(value = "프로필 요청")
     @GetMapping("/userprofiles")
     public CommonResponse<GetUserProfileDto> getProfiles(Authentication authentication) {
         GetUserProfileDto userProfile = userProfileService.getUserProfile(authentication);
         return CommonResponse.onSuccess(HttpStatus.OK.value(), userProfile);
     }
 
+    @ApiOperation(value = "프로필 수정 요청")
     @PutMapping("/userprofiles")
     public CommonResponse<GetUserProfileDto> updateProfile(Authentication authentication, @RequestBody PutUserProfileDto putUserProfileDto) {
         GetUserProfileDto userProfile = userProfileService.updateUserProfile(authentication, putUserProfileDto);
