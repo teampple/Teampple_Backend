09b31a400d32087801eb2ca2708f027a6d6e1dec
squirMM <jsm6616@naver.com>
2023-01-24 00:30:56 +0900

    :rocket: add constructor #62

--- src/main/java/Backend/teampple/global/error/exception/ForbiddenException.java
+++ src/main/java/Backend/teampple/global/error/exception/ForbiddenException.java
@@ -11,7 +11,7 @@ public class ForbiddenException extends BaseException {
     public ForbiddenException() {
         super(ErrorCode._BAD_REQUEST, ErrorCode._BAD_REQUEST.getMessage());
     }
-    public ForbiddenException(String message) {
-        super(ErrorCode._BAD_REQUEST, message);
+    public ForbiddenException(ErrorCode errorCode,String message) {
+        super(errorCode, message);
     }
 }
