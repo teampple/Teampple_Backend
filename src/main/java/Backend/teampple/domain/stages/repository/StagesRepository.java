f87d5791776015d7050993a6e56c712a0f5f6683
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-01-25 00:35:21 +0900

    🔨 fix(task) : task response dto 수정 #65

--- src/main/java/Backend/teampple/domain/stages/repository/StagesRepository.java
+++ src/main/java/Backend/teampple/domain/stages/repository/StagesRepository.java
@@ -3,12 +3,14 @@ package Backend.teampple.domain.stages.repository;
 import Backend.teampple.domain.stages.entity.Stage;
 import Backend.teampple.domain.teams.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface StagesRepository extends JpaRepository<Stage, Long> {
     List<Stage> findAllByTeamOrderBySequenceNum(Team team);
 
-    List<Stage> findAllByTeam(Team team);
+    @Query("select s from Stage s where s.team in :teams order by s.team.id")
+    List<Stage> findAllByTeamsOrderByTeamId(@Param("teams") List<Team> teams);
 }
