a4c52f81dffdc91b3cd68923f7318cabb24063f8
squirMM <jsm6616@naver.com>
2023-02-01 21:30:59 +0900

    :hammer: authentication principle cast to CustomUserDetails #81

--- src/main/java/Backend/teampple/domain/auth/oauth/OAuthSuccessHandler.java
+++ src/main/java/Backend/teampple/domain/auth/oauth/OAuthSuccessHandler.java
@@ -2,6 +2,7 @@ package Backend.teampple.domain.auth.oauth;
 
 import Backend.teampple.domain.auth.dto.JwtTokenDto;
 import Backend.teampple.domain.auth.jwt.JwtTokenProvider;
+import Backend.teampple.domain.auth.security.CustomUserDetails;
 import Backend.teampple.domain.users.entity.User;
 import Backend.teampple.domain.users.repository.UserRepository;
 import Backend.teampple.domain.users.service.UserService;
@@ -33,19 +34,25 @@ public class OAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         /**인증에 성공한 사용자*/
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        log.info("Principal에서 꺼낸 OAuth2User = {}", oAuth2User);
+        CustomUserDetails oAuth2User = (CustomUserDetails) authentication.getPrincipal();
+        log.info("Principal에서 꺼낸 OAuth2User = {}", oAuth2User.getUser().getKakaoId());
 
         /**JwtToken 생성*/
         JwtTokenDto jwtTokenDto = jwtTokenProvider.generateToken(authentication);
 
         /**RefreshToken update*/
-        userService.updateUserRefreshToken(oAuth2User.getName(), jwtTokenDto.getJwtRefreshToken(), jwtTokenDto.getExpRT());
+        if (oAuth2User.getUser().getRefreshToken() == null) {
+            userService.updateUserRefreshToken(oAuth2User.getUser(), jwtTokenDto.getJwtRefreshToken(), jwtTokenDto.getExpRT());
+        }
+
+        /**최초 가입자 확인*/
+        final boolean isNewUser = oAuth2User.getUser().getUserProfile() == null;
 
         /**JwtToken 과 함께 리다이렉트*/
         String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/api/oauth/kakao/success")
                 .queryParam("jwtAccessToken", jwtTokenDto.getJwtAccessToken())
                 .queryParam("jwtRefreshToken", jwtTokenDto.getJwtRefreshToken())
+                .queryParam("isNewUser", isNewUser)
                 .build().toUriString();
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
