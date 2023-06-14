41cb69b3ef6dcbc0b632b0b553cf3deca73d8a32
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-06-14 17:10:04 +0900

    feat: 레디스 엔티티 ttl 설정 변경

--- src/main/java/Backend/teampple/domain/auth/inmemory/RefreshToken.java
+++ src/main/java/Backend/teampple/domain/auth/inmemory/RefreshToken.java
@@ -5,12 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
 
 @Getter
 @NoArgsConstructor
-@RedisHash(value = "refreshToken", timeToLive = 1000L * 60 * 60 * 24 * 14)
+@RedisHash(value = "refreshToken")
 public class RefreshToken {
     @Id
     @Indexed
@@ -18,9 +19,13 @@ public class RefreshToken {
 
     private String authKey;
 
+    @TimeToLive
+    private Long ttl;
+
     @Builder
     public RefreshToken(String jwtRefreshToken, String authKey) {
         this.jwtRefreshToken = jwtRefreshToken;
         this.authKey = authKey;
+        this.ttl = 1000L * 60 * 60 * 24 * 14;
     }
 }
